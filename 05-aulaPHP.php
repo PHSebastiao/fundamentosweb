b23681b4bb089a3214cd488efc3a7961690c17d6
EA - Pedro Henrique Da Silva Sebastião <pedro_sebastiao@educadventista.org>
2023-04-20 20:26:06 -0300

    feat: primeira aula de php

---
+++ 05-aulaPHP.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+<h2>
+<?php
+echo "My first PHP script!";
+?></h2>
+
+<?php
+$txt = "PHP";
+echo "<h2>Eu amo $txt!</h2>";
+?>
+</body>
+</html>
