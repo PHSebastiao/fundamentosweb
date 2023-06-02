9480bf23d03713ab7fca7eb0cc3ad6e40f4bce58
PHSebastiao <phsilva.sebastiao@gmail.com>
2023-06-01 22:01:32 -0300

    feat: phpBase - estilização do login

--- phpBase/login.php
+++ phpBase/login.php
@@ -24,34 +24,23 @@ if(isset($_POST['sub'])){
       
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Login Sistema</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
     <body>
-        <h1> Login -alciomar </h1>
-        <form method="POST" enctype="multipart/form-data">
-            <table>
-                
-                <tr>
-                    <td>
-                        Username
-                        <input type="text" name="user">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        password
-                        <input type="password" name="pass">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="submit" name="sub" value="submit">
-                    </td>
-                    <td>
-                        <a href="reg.php">cadastrar</a>
-                    </td>
-                    
-                </tr>
-            </table>
+        <div class="container col-6">
+            <h1> Login </h1>
+            <form method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label>Usuário</label>
+                <input type="text" class="form-control" name="user">
+            </div>
+            <div class="form-group mb-3">
+                <label for="pass">Senha</label>
+                <input type="password" class="form-control" name="pass">
+            </div>
+            <button type="submit" name="sub" class="btn btn-primary">Enviar</button>
+            <a href="reg.php" class="btn btn-Secondary">cadastrar</a>
+        </div>
     </body>
 </html>
